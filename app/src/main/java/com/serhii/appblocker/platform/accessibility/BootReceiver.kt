a70323a4270673526a8ddc89a2df4269ca13ff9c
serhii <serhii.ost@gmail.com>
2026-03-30 21:28:23 +0200

    feat: add BootReceiver.kt to restart the service

---
+++ app/src/main/java/com/serhii/appblocker/platform/accessibility/BootReceiver.kt
@@ -0,0 +1,12 @@
+package com.serhii.appblocker.platform.accessibility
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+//        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+//        }
+    }
+}
