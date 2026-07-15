5eaead1254e3ac28acbb2f7f282ed7f423beb018
serhii <serhii.ost@gmail.com>
2026-07-15 21:52:32 +0200

    feat: add TERMS_AND_CONDITIONS.md, change timers and remove 1 minute option, version bump to 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.howlindev.appblocker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4"
+        versionCode = 6
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
