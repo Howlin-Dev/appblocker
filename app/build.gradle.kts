e84de3b1723443035438c4623f2defa3320b8669
serhii <serhii.ost@gmail.com>
2026-07-17 02:02:59 +0200

    chore: version code bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.howlindev.appblocker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
